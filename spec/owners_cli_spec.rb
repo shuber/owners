0dc16fdcb40cc521e236e493933b97a41019f0aa
Sean Huber <sean@lendinghome.com>
2015-11-24 23:29:18 -0800

    Get back to 100% code coverage

--- spec/owners_cli_spec.rb
+++ spec/owners_cli_spec.rb
@@ -1,16 +1,34 @@
 RSpec.describe Owners::CLI do
   describe "for" do
-    subject { described_class.new }
+    subject { capture { command } }
+    let(:command) { described_class.start(args) }
+    let(:args) { ["for", "example/app/controllers/users_controller.rb"] }
 
-    let(:command) { "bin/owners for #{path}" }
-    let(:path) { "example/app/controllers/users_controller.rb" }
-
-    it "parses owners correctly" do
-      expect(`#{command}`).to eq("@org/auth\n@org/blog\n")
+    def capture
+      stdout = $stdout
+      $stdout = StringIO.new
+      yield
+      $stdout.string
+    ensure
+      $stdout = stdout
     end
 
-    it "allows the OWNERS file to be changed" do
-      expect(`#{command} --file SOME_OTHER_FILE`).to eq("")
+    context "without a specified file" do
+      it "parses owners correctly" do
+        expect(subject).to eq("@org/auth\n@org/blog\n")
+      end
+    end
+
+    context "with a specified file" do
+      before { args << "--file" << "SOMETHING_ELSE" }
+
+      it "overrides the default OWNERS filename" do
+        begin
+          expect(subject).to eq("")
+        ensure
+          Owners.file = nil
+        end
+      end
     end
   end
 end
